5c2a96850b211d416453120c9b416e712cd91d40
Samuel Adewole <samadewole381@gmail.com>
2019-09-21 00:25:05 +0100

    build the npm finals

---
+++ yourbreak3/public/component---src-pages-index-js-bca7724327603749738f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{187:function(n,e,t){"use strict";t.r(e);var u=t(0),a=t.n(u);e.default=function(){return a.a.createElement("div",null)}}}]);
+//# sourceMappingURL=component---src-pages-index-js-bca7724327603749738f.js.map
